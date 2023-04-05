3539178ae5cfb41d4c077e813f5f19ce56f61384
geracons <martin13rc@gmail.com>
2023-04-05 15:49:36 +0200

    fix3

--- front/components/HeadPage.js
+++ front/components/HeadPage.js
@@ -5,8 +5,9 @@ import Head from 'next/head'
 export default function HeadPage() {
   return (
     <Head>
-    <title> Matemática de apoyo </title>
-    <link rel="icon" href="/favicon.ico" />
+      <title> Matemática de apoyo </title>
+      <link rel="icon" href="/favicon.ico" />
+      
     </Head>
   )
 }
--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -10,10 +10,13 @@ export default function Navbar() {
     gsap.from(navbarSection, 1, {opacity: 1, x:390, ease: Power3.easeOut, delay: .1})
   }, [])
 */
+
+
   return (
     <div className="">
+      
       <div className="w-100 h-16 bg-pink items-center">
-       <p className=" text-right text-white text-[28px] relative top-3 right-3"><a href="https://wa.me/34602661162" target="_blank">Contáctanos <WhatsAppIcon fontSize="28px" className="-mt-1"/></a></p>
+       <p className=" text-right text-white text-[28px] relative top-3 right-3"><a href="https://wa.me/34602661162" target="_blank">Contáctanos hoy <WhatsAppIcon fontSize="28px" className="-mt-1"/></a></p>
        <img src="/assets/logo.png" alt="logo" className=" w-60   absolute top-10 left-14   hidden md:block"/> 
        
         
